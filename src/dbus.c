574f0babb5b2d1a819e1a0856978b9837828dbba
Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
2011-01-21 18:15:40 +0200

    Proper D-Bus event loop for signal handling

--- src/dbus.c
+++ src/dbus.c
@@ -46,127 +46,387 @@
 
 #include <string.h>
 #include <stdint.h>
+#include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
+
+#include <time.h>
+#include <sys/poll.h>
+#include <sys/eventfd.h>
+#include <unistd.h>
 
 #include <at_dbus.h>
 #include <at_log.h>
 #include <at_thread.h>
 
+typedef struct
+{
+	DBusWatch *watch;
+	int fd;
+	unsigned events;
+} at_dbus_watch_t;
+
+typedef struct
+{
+	DBusTimeout *timeout;
+	unsigned interval;
+	uint64_t deadline;
+} at_dbus_timeout_t;
+
+typedef struct
+{
+	DBusConnection *conn;
+
+	size_t nfds;
+	at_dbus_watch_t **fds;
+	at_dbus_watch_t wakeup;
+
+	size_t ntos;
+	at_dbus_timeout_t **tos;
+
+	pthread_mutex_t lock;
+	pthread_t thread;
+} at_dbus_t;
+
+static void at_dbus_wakeup (void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	uint64_t count = 1;
+
+	write (ad->wakeup.fd, &count, sizeof (count));
+}
+
+static void at_dbus_toggle_watch (DBusWatch *watch, void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	at_dbus_watch_t *adw = dbus_watch_get_data (watch);
+	unsigned events = 0;
+
+	if (dbus_watch_get_enabled (watch))
+	{
+		unsigned flags = dbus_watch_get_flags (watch);
+
+		if (flags & DBUS_WATCH_READABLE)
+			events |= POLLIN;
+		if (flags & DBUS_WATCH_WRITABLE)
+			events |= POLLOUT;
+	}
+
+	pthread_mutex_lock (&ad->lock);
+	assert (adw->fd == dbus_watch_get_unix_fd (watch));
+	adw->events = events;
+	pthread_mutex_unlock (&ad->lock);
+}
+
+static dbus_bool_t at_dbus_add_watch (DBusWatch *watch, void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	at_dbus_watch_t *adw = malloc (sizeof (*adw));
+	if (adw == NULL)
+		return FALSE;
+
+	adw->watch = watch;
+	adw->fd = dbus_watch_get_unix_fd (watch);
+	adw->events = 0;
+	dbus_watch_set_data (watch, adw, NULL);
+
+	pthread_mutex_lock (&ad->lock);
+
+	size_t n = ad->nfds;
+	at_dbus_watch_t **fds = realloc (ad->fds, (n + 1) * sizeof (*fds));
+	if (fds != NULL)
+	{
+		ad->fds = fds;
+		fds[n++] = adw;
+		ad->nfds = n;
+	}
+
+	pthread_mutex_unlock (&ad->lock);
+	return (fds != NULL);
+}
+
+static void at_dbus_remove_watch (DBusWatch *watch, void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	at_dbus_watch_t *adw = dbus_watch_get_data (watch);
+
+	pthread_mutex_lock (&ad->lock);
+	at_dbus_watch_t **fds = ad->fds;
+
+	size_t i, n = ad->nfds;
+	for (i = 0; fds[i] != adw; i++)
+		assert (i < n);
+
+	ad->nfds = --n;
+	memmove (fds + i, fds + i + 1, sizeof (*fds) * (n - i));
+	fds = realloc (fds, sizeof (*fds) * n);
+	if (fds != NULL)
+		ad->fds = fds;
+	pthread_mutex_unlock (&ad->lock);
+
+	free (adw);
+}
+
+static uint64_t getclock (void)
+{
+	struct timespec ts;
+
+	clock_gettime (CLOCK_MONOTONIC, &ts);
+	return ts.tv_sec * UINT64_C(1000000000) + ts.tv_nsec;
+}
+
+static void at_dbus_toggle_timeout (DBusTimeout *timeout, void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	at_dbus_timeout_t *adt = dbus_timeout_get_data (timeout);
+	uint64_t deadline;
+
+	if (dbus_timeout_get_enabled (timeout))
+		deadline = getclock () + dbus_timeout_get_interval (timeout);
+	else
+		deadline = 0;
+
+	pthread_mutex_lock (&ad->lock);
+	assert (adt->interval == (unsigned)dbus_timeout_get_interval (timeout));
+	adt->deadline = deadline;
+	pthread_mutex_unlock (&ad->lock);
+}
+
+static dbus_bool_t at_dbus_add_timeout (DBusTimeout *timeout, void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	at_dbus_timeout_t *adt = malloc (sizeof (*adt));
+	if (adt == NULL)
+		return FALSE;
+
+	adt->timeout = timeout;
+	adt->interval = dbus_timeout_get_interval (timeout);
+	adt->deadline = 0;
+	dbus_timeout_set_data (timeout, adt, NULL);
+
+	pthread_mutex_lock (&ad->lock);
+
+	size_t n = ad->ntos;
+	at_dbus_timeout_t **tos = realloc (ad->tos, (n + 1) * sizeof (*tos));
+	if (tos != NULL)
+	{
+		ad->tos = tos;
+		tos[n++] = adt;
+		ad->ntos = n;
+	}
+
+	pthread_mutex_unlock (&ad->lock);
+	return (tos != NULL);
+}
+
+static void at_dbus_remove_timeout (DBusTimeout *timeout, void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	at_dbus_timeout_t *adt = dbus_timeout_get_data (timeout);
+
+	pthread_mutex_lock (&ad->lock);
+	at_dbus_timeout_t **tos = ad->tos;
+
+	size_t i, n = ad->ntos;
+	for (i = 0; tos[i] != adt; i++)
+		assert (i < n);
+
+	ad->ntos = --n;
+	if (n > 0)
+	{
+		memmove (tos + i, tos + i + 1, sizeof (*tos) * (n - i));
+		tos = realloc (tos, sizeof (*tos) * n);
+		if (tos != NULL)
+			ad->tos = tos;
+	}
+	else
+	{
+		free (tos);
+		ad->tos = NULL;
+	}
+	pthread_mutex_unlock (&ad->lock);
+
+	free (adt);
+}
+
+static void *at_dbus_thread (void *opaque)
+{
+	at_dbus_t *ad = opaque;
+	DBusConnection *conn = ad->conn;
+
+loop:
+	for (;;)
+	{
+		int canc = at_cancel_disable ();
+		while (dbus_connection_get_dispatch_status (conn)
+				== DBUS_DISPATCH_DATA_REMAINS)
+			dbus_connection_dispatch (conn);
+		at_cancel_enable (canc);
+
+		uint64_t now = getclock ();
+
+		pthread_mutex_lock (&ad->lock);
+		size_t nfds = ad->nfds;
+		size_t ntos = ad->ntos;
+		struct pollfd ufd[nfds];
+		int timeout = -1;
+
+		for (size_t i = 0; i < nfds; i++)
+		{
+			at_dbus_watch_t *adw = ad->fds[i];
+
+			ufd[i].fd = adw->fd;
+			ufd[i].events = adw->events;
+		}
+
+		for (size_t i = 0; i < ntos; i++)
+		{
+			at_dbus_timeout_t *adt = ad->tos[i];
+
+			if (adt->deadline == 0)
+				continue;
+
+			int64_t delta = now - adt->deadline;
+			if (delta <= 0)
+			{
+				adt->deadline += adt->interval;
+				pthread_mutex_unlock (&ad->lock);
+				dbus_timeout_handle (adt->timeout);
+				goto loop;
+			}
+			if (timeout < 0 || delta < timeout)
+				timeout = delta;
+		}
+
+		pthread_mutex_unlock (&ad->lock);
+
+		int n = poll (ufd, nfds, timeout);
+		if (n == -1)
+			continue;
+
+		pthread_mutex_lock (&ad->lock);
+		nfds = ad->nfds;
+		for (size_t i = 0; n > 0; i++)
+		{
+			if (ufd[i].revents == 0)
+				continue;
+
+			int fd = ufd[i].fd;
+			unsigned flags = 0;
+			if (ufd[i].revents & POLLIN)
+				flags |= DBUS_WATCH_READABLE;
+			if (ufd[i].revents & POLLOUT)
+				flags |= DBUS_WATCH_WRITABLE;
+			if (ufd[i].revents & POLLERR)
+				flags |= DBUS_WATCH_ERROR;
+			if (ufd[i].revents & POLLHUP)
+				flags |= DBUS_WATCH_HANGUP;
+
+			/* Unfortunately, the order of watches could have changed */
+			for (size_t j = 0; j < nfds; j++)
+			{
+				at_dbus_watch_t *adw = ad->fds[j];
+
+				if (adw->fd == fd)
+				{
+					pthread_mutex_unlock (&ad->lock);
+					if (adw == &ad->wakeup)
+						read (fd, &(uint64_t){ 0 }, 8);
+					else
+						dbus_watch_handle (adw->watch, flags);
+					goto loop;
+				}
+			}	
+			n--;
+		}
+		pthread_mutex_unlock (&ad->lock);
+	}
+	assert (0);
+}
+
 /* Note: Cleanup is not implemented currently. This would become a problem if
  * some process would dlclose() libmatd. Currently, this is not the case.
  */
 
-static DBusConnection *at_dbus_connect_unlocked (DBusBusType type)
-{
-	if (!dbus_threads_init_default ())
-		return NULL;
-
-	DBusError err;
-	dbus_error_init (&err);
-
-	DBusConnection *conn = dbus_bus_get_private (type, &err);
-	if (conn == NULL)
-	{
-		error ("Cannot connect to system D-Bus (%s)",
-		       err.message ? err.message : "");
-		dbus_error_free (&err);
-		return NULL;
-	}
-	dbus_connection_set_exit_on_disconnect (conn, FALSE);
-	return conn;
-}
-
-#ifdef NEED_SIGNALS
-static void *at_dbus_thread (void *data)
-{
-	DBusConnection *conn = data;
-
-	while (dbus_connection_read_write_dispatch (conn, -1));
-
-	return NULL;
-}
-
-static DBusConnection *at_dbus_get (DBusBusType type, bool signals)
-#else
 static DBusConnection *at_dbus_get (DBusBusType type)
-#endif
 {
-	static DBusConnection *system_bus = NULL;
-#ifdef NEED_SIGNALS
-	static DBusConnection *system_bus_async = NULL;
-#endif
-	static DBusConnection *session_bus = NULL;
+	static at_dbus_t system_bus = { .conn = NULL };
+	static at_dbus_t session_bus = { .conn = NULL };
 	static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-	DBusConnection *restrict *pconn, *conn;
+	at_dbus_t *restrict bus;
+	DBusConnection *conn;
 
 	switch (type)
 	{
 		case DBUS_BUS_SYSTEM:
-#ifdef NEED_SIGNALS
-			pconn = signals ? &system_bus_async : &system_bus;
-#else
-			pconn = &system_bus;
-#endif
+			bus = &system_bus;
 			break;
 		case DBUS_BUS_SESSION:
-#ifdef NEED_SIGNALS
-			if (signals)
-				return NULL;
-#endif
-			pconn = &session_bus;
+			bus = &session_bus;
 			break;
 		default:
 			return NULL;
 	}
 
 	pthread_mutex_lock (&lock);
-	conn = *pconn;
+	conn = bus->conn;
+	if (conn != NULL)
+		goto out;
+
+	/* Connect to DBus first time */
+	if (!dbus_threads_init_default ())
+		goto out;
+
+	DBusError err;
+	dbus_error_init (&err);
+	conn = dbus_bus_get_private (type, &err);
 	if (conn == NULL)
 	{
-		/* Connect to DBus first time */
-		conn = at_dbus_connect_unlocked (type);
-#ifdef NEED_SIGNALS
-		/* Initialize DBus signal handling thread */
-		if (conn != NULL && signals)
-		{
-			pthread_t th;
-
-			if (at_thread_create (&th, at_dbus_thread, conn))
-			{
-				dbus_connection_unref (conn);
-				conn = NULL;
-			}
-			else
-				pthread_detach (th);
-		}
-#endif
-		*pconn = conn;
+		error ("Cannot connect to system D-Bus (%s)",
+		       err.message ? err.message : "");
+		dbus_error_free (&err);
+		goto out;
 	}
+
+	bus->conn = conn;
+	dbus_connection_set_exit_on_disconnect (conn, FALSE);
+
+	/* Initialize DBus signal handling thread */
+	bus->nfds = 1;
+	bus->fds = malloc (sizeof (bus->fds[0]));
+	if (bus->fds == NULL)
+		goto drop;
+
+	bus->wakeup.watch = NULL;
+	bus->wakeup.fd = eventfd (0, EFD_CLOEXEC);
+	if (bus->wakeup.fd == -1)
+		goto drop;
+	bus->fds[0] = &bus->wakeup;
+
+	bus->ntos = 0;
+	bus->tos = NULL;
+
+	pthread_mutex_init (&bus->lock, NULL);
+	dbus_connection_set_wakeup_main_function (conn, at_dbus_wakeup, bus, NULL);
+	if (!dbus_connection_set_watch_functions (conn, at_dbus_add_watch,
+	                     at_dbus_remove_watch, at_dbus_toggle_watch, bus, NULL)
+	 || !dbus_connection_set_timeout_functions (conn, at_dbus_add_timeout,
+	                 at_dbus_remove_timeout, at_dbus_toggle_timeout, bus, NULL)
+	 || at_thread_create (&bus->thread, at_dbus_thread, bus))
+	{
+		close (bus->wakeup.fd);
+		goto drop;
+	}
+
+out:
 	pthread_mutex_unlock (&lock);
 	return conn;
-}
 
-/**
- * Connect to a given DBus bus (if not already done).
- * @return the DBus connection or NULL on error.
- */
-static DBusConnection *at_dbus_connect (DBusBusType type)
-{
-#ifndef NEED_SIGNALS
-	return at_dbus_get (type);
-#else
-	return at_dbus_get (type, false);
-}
-
-/**
- * Connect to a given bus (if not already done), and start the read, write and
- * dispatch procedures asynchronously.
- * @return the DBus connection or NULL on error.
- */
-static DBusConnection *at_dbus_connect_dispatch (DBusBusType type)
-{
-	return at_dbus_get (type, true);
+drop:
+	dbus_connection_unref (conn);
+	free (bus->fds);
+	conn = NULL;
+	goto out;
 }
 
 
@@ -176,7 +436,7 @@ int at_dbus_add_filter (DBusBusType bus, DBusHandleMessageFunction filter_cb,
 	int canc = at_cancel_disable ();
 	int ret = -1;
 
-	DBusConnection *conn = at_dbus_connect_dispatch (bus);
+	DBusConnection *conn = at_dbus_get (bus);
 	if (conn != NULL
 	 && dbus_connection_add_filter (conn, filter_cb, opaque, free_cb))
 		ret = 0;
@@ -190,7 +450,7 @@ void at_dbus_remove_filter (DBusBusType bus,
                             DBusHandleMessageFunction filter_cb, void *opaque)
 {
 	int canc = at_cancel_disable ();
-	DBusConnection *conn = at_dbus_connect_dispatch (bus);
+	DBusConnection *conn = at_dbus_get (bus);
 	assert (conn != NULL); /* cannot fail as add_filter succeeded */
 
 	dbus_connection_remove_filter (conn, filter_cb, opaque);
@@ -200,7 +460,7 @@ void at_dbus_remove_filter (DBusBusType bus,
 
 void at_dbus_add_match (DBusBusType bus, const char *rule)
 {
-	DBusConnection *conn = at_dbus_connect_dispatch (bus);
+	DBusConnection *conn = at_dbus_get (bus);
 	if (conn == NULL)
 		return;
 
@@ -210,12 +470,11 @@ void at_dbus_add_match (DBusBusType bus, const char *rule)
 
 void at_dbus_remove_match (DBusBusType bus, const char *rule)
 {
-	DBusConnection *conn = at_dbus_connect_dispatch (bus);
+	DBusConnection *conn = at_dbus_get (bus);
 	if (conn == NULL)
 		return;
 
 	dbus_bus_remove_match (conn, rule, NULL);
-#endif
 }
 
 
@@ -225,7 +484,7 @@ int at_dbus_request (DBusBusType bus, DBusMessage *msg)
 {
 	at_cancel_assert (false);
 
-	DBusConnection *conn = at_dbus_connect (bus);
+	DBusConnection *conn = at_dbus_get (bus);
 	if (conn == NULL)
 		return -1;
 
@@ -263,7 +522,7 @@ DBusMessage *at_dbus_query (DBusBusType bus, DBusMessage *req, int delay,
 
 	dbus_error_init (err);
 
-	DBusConnection *conn = at_dbus_connect (bus);
+	DBusConnection *conn = at_dbus_get (bus);
 	if (conn == NULL)
 		return NULL;
 
