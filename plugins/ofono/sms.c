e6d95ecff32504d3feea3f314ae01bc29493241f
Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
2011-02-08 14:58:26 +0200

    oFono: implement AT+CSCA

--- plugins/ofono/sms.c
+++ plugins/ofono/sms.c
@@ -112,7 +112,66 @@ static at_error_t handle_cgsms (at_modem_t *modem, const char *req,
 	return at_setting (modem, req, data, set_cgsms, get_cgsms, list_cgsms);
 }
 
+
+/*** AT+CSCA ***/
+
+static at_error_t set_csca (at_modem_t *modem, const char *req, void *data)
+{
+	plugin_t *p = data;
+	char number[256];
+	unsigned type;
+
+	(void) modem;
+
+	switch (sscanf (req, " \"%255[^\"]\" , %u", number, &type))
+	{
+		case 2:
+			if (type != (*number == '+') ? 145 : 129)
+				return AT_CME_ENOTSUP;
+			/* fallthrough */
+		case 1:
+			break;
+		default:
+			return AT_CME_EINVAL;
+	}
+
+	return modem_prop_set_string (p, "MessageManager", "ServiceCenterAddress",
+	                              number);
+}
+
+static at_error_t get_csca (at_modem_t *modem, void *data)
+{
+	plugin_t *p = data;
+	char *number = modem_prop_get_string (p, "MessageManager",
+	                                      "ServiceCenterAddress");
+
+	if (number == NULL)
+		return AT_CME_UNKNOWN;
+
+	at_intermediate (modem, "\r\n+CSCA: \"%s\",%u", number,
+	                 (*number == '+') ? 145 : 129);
+	free (number);
+	return AT_OK;
+}
+
+static at_error_t list_csca (at_modem_t *modem, void *data)
+{
+	(void) modem;
+	(void) data;
+	return AT_OK;
+}
+
+static at_error_t handle_csca (at_modem_t *modem, const char *req,
+                                 void *data)
+{
+	return at_setting (modem, req, data, set_csca, get_csca, list_csca);
+}
+
+
+/*** Registration ***/
+
 void sms_register (at_commands_t *set, plugin_t *p)
 {
 	at_register (set, "+CGSMS", handle_cgsms, p);
+	at_register (set, "+CSCA", handle_csca, p);
 }
