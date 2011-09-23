ef06b07f1f2e3ea462685cde4e12c8971de2f585
Rémi Denis-Courmont <remi.denis-courmont@nokia.com>
2011-09-23 18:45:47 +0300

    Fix D-Bus event handling

--- src/dbus.c
+++ src/dbus.c
@@ -46,6 +46,7 @@
 
 #include <string.h>
 #include <stdint.h>
+#include <limits.h>
 #include <stdlib.h>
 #include <stdbool.h>
 #include <assert.h>
@@ -59,34 +60,48 @@
 #include <at_log.h>
 #include <at_thread.h>
 
-typedef struct
+typedef struct at_dbus_watch at_dbus_watch_t;
+typedef struct at_dbus_timeout at_dbus_timeout_t;
+
+struct at_dbus_watch
 {
+	at_dbus_watch_t *prev, *next;
 	DBusWatch *watch;
 	int fd;
 	unsigned events;
-} at_dbus_watch_t;
+};
 
-typedef struct
+struct at_dbus_timeout
 {
+	at_dbus_timeout_t *prev, *next;
 	DBusTimeout *timeout;
 	unsigned interval;
 	uint64_t deadline;
-} at_dbus_timeout_t;
+};
 
 typedef struct
 {
-	DBusConnection *conn;
-
-	size_t nfds;
-	at_dbus_watch_t **fds;
-	at_dbus_watch_t wakeup;
-
-	size_t ntos;
-	at_dbus_timeout_t **tos;
-
 	pthread_mutex_t lock;
 	pthread_t thread;
+
+	DBusConnection *conn;
+
+	struct
+	{
+		int fd;
+	} wakeup;
+	struct
+	{
+		size_t n;
+		at_dbus_watch_t *first;
+	} watch;
+	struct
+	{
+		at_dbus_timeout_t *first;
+	} timeout;
 } at_dbus_t;
+
+#define BUS_INIT { .lock = PTHREAD_MUTEX_INITIALIZER, .conn = NULL, }
 
 static void at_dbus_wakeup (void *opaque)
 {
@@ -112,10 +127,13 @@ static void at_dbus_toggle_watch (DBusWatch *watch, void *opaque)
 			events |= POLLOUT;
 	}
 
+	debug ("WATCH %d: %s %s", adw->fd,
+	       (events & POLLIN) ? "READ" : "", (events & POLLOUT) ? "WRITE" : "");
 	pthread_mutex_lock (&ad->lock);
 	assert (adw->fd == dbus_watch_get_unix_fd (watch));
 	adw->events = events;
 	pthread_mutex_unlock (&ad->lock);
+	at_dbus_wakeup (ad);
 }
 
 static dbus_bool_t at_dbus_add_watch (DBusWatch *watch, void *opaque)
@@ -125,24 +143,25 @@ static dbus_bool_t at_dbus_add_watch (DBusWatch *watch, void *opaque)
 	if (adw == NULL)
 		return FALSE;
 
+	adw->prev = NULL;
 	adw->watch = watch;
 	adw->fd = dbus_watch_get_unix_fd (watch);
 	adw->events = 0;
 	dbus_watch_set_data (watch, adw, NULL);
 
 	pthread_mutex_lock (&ad->lock);
+	ad->watch.n++;
 
-	size_t n = ad->nfds;
-	at_dbus_watch_t **fds = realloc (ad->fds, (n + 1) * sizeof (*fds));
-	if (fds != NULL)
+	adw->next = ad->watch.first;
+	ad->watch.first = adw;
+	if (adw->next != NULL)
 	{
-		ad->fds = fds;
-		fds[n++] = adw;
-		ad->nfds = n;
+		assert (adw->next->prev == NULL);
+		adw->next->prev = adw;
 	}
-
 	pthread_mutex_unlock (&ad->lock);
-	return (fds != NULL);
+	at_dbus_toggle_watch (watch, opaque);
+	return TRUE;
 }
 
 static void at_dbus_remove_watch (DBusWatch *watch, void *opaque)
@@ -151,17 +170,21 @@ static void at_dbus_remove_watch (DBusWatch *watch, void *opaque)
 	at_dbus_watch_t *adw = dbus_watch_get_data (watch);
 
 	pthread_mutex_lock (&ad->lock);
-	at_dbus_watch_t **fds = ad->fds;
+	assert (ad->watch.n > 0);
+	ad->watch.n--;
 
-	size_t i, n = ad->nfds;
-	for (i = 0; fds[i] != adw; i++)
-		assert (i < n);
-
-	ad->nfds = --n;
-	memmove (fds + i, fds + i + 1, sizeof (*fds) * (n - i));
-	fds = realloc (fds, sizeof (*fds) * n);
-	if (fds != NULL)
-		ad->fds = fds;
+	if (adw->prev != NULL)
+	{
+		assert (ad->watch.first != adw);
+		adw->prev->next = adw->next;
+	}
+	else
+	{
+		assert (ad->watch.first == adw);
+		ad->watch.first = adw->next;
+	}
+	if (adw->next != NULL)
+		adw->next->prev = adw->prev;
 	pthread_mutex_unlock (&ad->lock);
 
 	free (adw);
@@ -190,6 +213,8 @@ static void at_dbus_toggle_timeout (DBusTimeout *timeout, void *opaque)
 	assert (adt->interval == (unsigned)dbus_timeout_get_interval (timeout));
 	adt->deadline = deadline;
 	pthread_mutex_unlock (&ad->lock);
+
+	at_dbus_wakeup (ad);
 }
 
 static dbus_bool_t at_dbus_add_timeout (DBusTimeout *timeout, void *opaque)
@@ -199,24 +224,23 @@ static dbus_bool_t at_dbus_add_timeout (DBusTimeout *timeout, void *opaque)
 	if (adt == NULL)
 		return FALSE;
 
+	adt->prev = NULL;
 	adt->timeout = timeout;
 	adt->interval = dbus_timeout_get_interval (timeout);
 	adt->deadline = 0;
 	dbus_timeout_set_data (timeout, adt, NULL);
 
 	pthread_mutex_lock (&ad->lock);
-
-	size_t n = ad->ntos;
-	at_dbus_timeout_t **tos = realloc (ad->tos, (n + 1) * sizeof (*tos));
-	if (tos != NULL)
+	adt->next = ad->timeout.first;
+	ad->timeout.first = adt;
+	if (adt->next != NULL)
 	{
-		ad->tos = tos;
-		tos[n++] = adt;
-		ad->ntos = n;
+		assert (adt->next->prev == NULL);
+		adt->next->prev = adt;
 	}
 
 	pthread_mutex_unlock (&ad->lock);
-	return (tos != NULL);
+	return TRUE;
 }
 
 static void at_dbus_remove_timeout (DBusTimeout *timeout, void *opaque)
@@ -225,25 +249,18 @@ static void at_dbus_remove_timeout (DBusTimeout *timeout, void *opaque)
 	at_dbus_timeout_t *adt = dbus_timeout_get_data (timeout);
 
 	pthread_mutex_lock (&ad->lock);
-	at_dbus_timeout_t **tos = ad->tos;
-
-	size_t i, n = ad->ntos;
-	for (i = 0; tos[i] != adt; i++)
-		assert (i < n);
-
-	ad->ntos = --n;
-	if (n > 0)
+	if (adt->prev != NULL)
 	{
-		memmove (tos + i, tos + i + 1, sizeof (*tos) * (n - i));
-		tos = realloc (tos, sizeof (*tos) * n);
-		if (tos != NULL)
-			ad->tos = tos;
+		assert (ad->timeout.first != adt);
+		adt->prev->next = adt->next;
 	}
 	else
 	{
-		free (tos);
-		ad->tos = NULL;
+		assert (ad->timeout.first == adt);
+		ad->timeout.first = adt->next;
 	}
+	if (adt->next != NULL)
+		adt->next->prev = adt->prev;
 	pthread_mutex_unlock (&ad->lock);
 
 	free (adt);
@@ -264,49 +281,73 @@ loop:
 		at_cancel_enable (canc);
 
 		uint64_t now = getclock ();
-
-		pthread_mutex_lock (&ad->lock);
-		size_t nfds = ad->nfds;
-		size_t ntos = ad->ntos;
-		struct pollfd ufd[nfds];
 		int timeout = -1;
 
-		for (size_t i = 0; i < nfds; i++)
+		pthread_mutex_lock (&ad->lock);
+		/* Handle timers */
+		for (at_dbus_timeout_t *adt = ad->timeout.first;
+		     adt != NULL;
+		     adt = adt->next)
 		{
-			at_dbus_watch_t *adw = ad->fds[i];
-
-			ufd[i].fd = adw->fd;
-			ufd[i].events = adw->events;
-		}
-
-		for (size_t i = 0; i < ntos; i++)
-		{
-			at_dbus_timeout_t *adt = ad->tos[i];
-
 			if (adt->deadline == 0)
 				continue;
 
 			int64_t delta = now - adt->deadline;
 			if (delta <= 0)
 			{
+				/* FIXME: deal with drift */
 				adt->deadline += adt->interval;
 				pthread_mutex_unlock (&ad->lock);
 				dbus_timeout_handle (adt->timeout);
 				goto loop;
 			}
+			delta /= 1000000;
 			if (timeout < 0 || delta < timeout)
-				timeout = delta;
+			{
+				if (delta > INT_MAX)
+					timeout = INT_MAX;
+				else
+					timeout = delta;
+			}
 		}
 
+		/* Register file descriptor events */
+		struct pollfd ufd[1 + ad->watch.n], *p = ufd + 1;
+		for (const at_dbus_watch_t *adw = ad->watch.first;
+		     adw != NULL;
+		     adw = adw->next)
+		{
+			if (p->events == 0)
+				continue;
+
+			p->fd = adw->fd;
+			p->events = adw->events;
+			debug ("POLL %d: %s %s", adw->fd,
+			       (adw->events & POLLIN) ? "READ" : "",
+			       (adw->events & POLLOUT) ? "WRITE" : "");
+			p++;
+		}
 		pthread_mutex_unlock (&ad->lock);
 
-		int n = poll (ufd, nfds, timeout);
+		assert ((p - ufd) <= (ptrdiff_t)(sizeof (ufd) / sizeof (ufd[0])));
+		/* Register wake-up event */
+		ufd[0].fd = ad->wakeup.fd;
+		ufd[0].events = POLLIN;
+
+		int n = poll (ufd, p - ufd, timeout);
 		if (n == -1)
-			continue;
+			continue; /* signal */
+
+		/* Handle wake-up event */
+		if (ufd[0].revents)
+		{
+			read (ufd[0].fd, &(uint64_t){ 0 }, 8);
+			continue; /* wake-up */
+		}
 
 		pthread_mutex_lock (&ad->lock);
-		nfds = ad->nfds;
-		for (size_t i = 0; n > 0; i++)
+		/* Handle file descriptor events */
+		for (size_t i = 1; n > 0; i++)
 		{
 			if (ufd[i].revents == 0)
 				continue;
@@ -323,18 +364,16 @@ loop:
 				flags |= DBUS_WATCH_HANGUP;
 
 			/* Unfortunately, the order of watches could have changed */
-			for (size_t j = 0; j < nfds; j++)
+			for (at_dbus_watch_t *adw = ad->watch.first;
+			     adw != NULL;
+			     adw = adw->next)
 			{
-				at_dbus_watch_t *adw = ad->fds[j];
-
 				if (adw->fd == fd)
 				{
 					pthread_mutex_unlock (&ad->lock);
-					if (adw == &ad->wakeup)
-						read (fd, &(uint64_t){ 0 }, 8);
-					else
-						dbus_watch_handle (adw->watch, flags);
-					goto loop;
+					dbus_watch_handle (adw->watch, flags);
+					pthread_mutex_lock (&ad->lock);
+					break;
 				}
 			}	
 			n--;
@@ -350,8 +389,8 @@ loop:
 
 static DBusConnection *at_dbus_get (DBusBusType type)
 {
-	static at_dbus_t system_bus = { .conn = NULL };
-	static at_dbus_t session_bus = { .conn = NULL };
+	static at_dbus_t system_bus = BUS_INIT;
+	static at_dbus_t session_bus = BUS_INIT;
 	static pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 	at_dbus_t *restrict bus;
 	DBusConnection *conn;
@@ -388,25 +427,13 @@ static DBusConnection *at_dbus_get (DBusBusType type)
 		goto out;
 	}
 
-	bus->conn = conn;
 	dbus_connection_set_exit_on_disconnect (conn, FALSE);
 
 	/* Initialize DBus signal handling thread */
-	bus->nfds = 1;
-	bus->fds = malloc (sizeof (bus->fds[0]));
-	if (bus->fds == NULL)
-		goto drop;
-
-	bus->wakeup.watch = NULL;
 	bus->wakeup.fd = eventfd (0, EFD_CLOEXEC);
 	if (bus->wakeup.fd == -1)
 		goto drop;
-	bus->fds[0] = &bus->wakeup;
 
-	bus->ntos = 0;
-	bus->tos = NULL;
-
-	pthread_mutex_init (&bus->lock, NULL);
 	dbus_connection_set_wakeup_main_function (conn, at_dbus_wakeup, bus, NULL);
 	if (!dbus_connection_set_watch_functions (conn, at_dbus_add_watch,
 	                     at_dbus_remove_watch, at_dbus_toggle_watch, bus, NULL)
@@ -417,14 +444,13 @@ static DBusConnection *at_dbus_get (DBusBusType type)
 		close (bus->wakeup.fd);
 		goto drop;
 	}
-
+	bus->conn = conn;
 out:
 	pthread_mutex_unlock (&lock);
 	return conn;
 
 drop:
 	dbus_connection_unref (conn);
-	free (bus->fds);
 	conn = NULL;
 	goto out;
 }
